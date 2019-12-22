4a5662438339063ae791687e606cfa9d78c5f3a3
hannn0919 <44270736+hannn0919@users.noreply.github.com>
2019-12-22 15:34:49 +0800

    update home html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,49 +8,55 @@
         <div class = "col-md-8 col-lg-8 col-sm-8 col-xs-12">
             <br>
             <div class="card" style="height:85vh;">
-                <div class="card-header bg-white font-weight-bold" style="height: auto;">課程
+                <div class="card-header bg-white font-weight-bold" style="height: auto;">課程清單
                 </div>
                 <div class="card-body overflow-auto" >
-
-                    @foreach($array_data['course'] as $key => $d)
-                        <div class="card" style="width: 100%;">
-                            <div class="card-header">
-                                <a href="#">
-                                    <div style = "display:inline;">{{$d->CourseNo}}</div>
-                                    <div style = "display:inline;float:right;">{{$d->CourseTitle}}</div>
-                                </a>
-                            </div>
-                            <div class="card-body">
-                                <div class = "col-md-4"style="display:inline;color: black; text-decoration:none;">
-                                @switch($d->Type)
-                                    @case ("1")
-                                        必修
-                                        @break;
-                                    @case ("2")
-                                        選修
-                                        @break;
-                                    @case ("3")
-                                        通識
-                                        @break;
-                                    @case ("4")
-                                        體育
-                                        @break;
-                                    @case ("5")
-                                        語文
-                                        @break;
-                                    @case ("6")
-                                        其他
-                                        @break;
-                                    @default
-                                        something got wrong
-                                @endswitch
-                                </div>
-                                <div class = "col-md-4"style="display:inline;color: black; text-decoration:none;">{{$d->Instructor}}</div>
-                                <div class = "col-md-4"style="display:inline;color: black; text-decoration:none;">{{$d->Department}}</div>
-                            </div>
-                        </div>
-                    @endforeach 
-
+                    
+                            <table class="table table-condensed table-hover text-center">
+                                <thead>
+                                <tr class="thead-dark">
+                                    <th scope="col" style="vertical-align: middle;">課號</th>
+                                    <th scope="col" style="vertical-align: middle;">課名</th>
+                                    <th scope="col" style="vertical-align: middle;">授課老師</th>
+                                    <th scope="col" style="vertical-align: middle;">開課單位</th>
+                                    <th scope="col" style="vertical-align: middle;">課程類型</th>
+                                </tr>
+                                </thead>
+                                <tbody class="bg-white">
+                                    @foreach($array_data['course'] as $key => $d)
+                                    <tr onclick="window.location='/course/{{$d->CourseNo}}'">
+                                        <th scope="row">{{$d->CourseNo}}</th>
+                                        <td>{{$d->CourseTitle}}</td>
+                                        <td>{{$d->Instructor}}</td>
+                                        <td>{{$d->Department}}</td>
+                                        <td>@switch($d->Type)
+                                                @case ("1")
+                                                    必修
+                                                    @break;
+                                                @case ("2")
+                                                    選修
+                                                    @break;
+                                                @case ("3")
+                                                    通識
+                                                    @break;
+                                                @case ("4")
+                                                    體育
+                                                    @break;
+                                                @case ("5")
+                                                    語文
+                                                    @break;
+                                                @case ("6")
+                                                    其他
+                                                    @break;
+                                                @default
+                                                    something got wrong
+                                            @endswitch
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                       
                 </div>
             </div>
         </div>
