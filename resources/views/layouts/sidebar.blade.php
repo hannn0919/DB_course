a9695ddba76e9017d6c365145adba57d1222ee94
hannn0919 <44270736+hannn0919@users.noreply.github.com>
2019-12-24 02:18:32 +0800

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,12 +3,29 @@
         <br>
         <br>
             <div>
-                <ul class="list-group"><a href = "{{route('分類',['資訊工程學系','Department'])}}" style= "color:white;">資工系</a></ul>
+                <ul class="list-group"><a href = "{{route('分類',['資訊工程學系','Department'])}}" class="cato">資工系</a></ul>
                 <br>
-                <ul class="list-group"><a href="{{route('分類',['3','Type'])}}" style ="color:white;">通識</a></ul>
+                <ul class="list-group"><a href="{{route('分類',['3','Type'])}}" class="cato">通識</a></ul>
                 <br>
-                <ul class="list-group"><a href="{{route('分類',['4','Type'])}}" style ="color:white;">體育</a></ul>
+                <ul class="list-group"><a href="{{route('分類',['4','Type'])}}" class="cato">體育</a></ul>
                 <br>
-                <ul class="list-group"><a href="{{route('分類',['5','Type'])}}" style ="color:white;">語文</a></ul>
+                <ul class="list-group"><a href="{{route('分類',['5','Type'])}}" class="cato">語文</a></ul>
+                <br>
+                <ul class="list-group"><a href="{{route('分類',['6','Type'])}}" class="cato">其他</a></ul>
             </div>
     </div>
+
+    <style type="text/CSS">
+        .cato {
+            color:white;
+            text-align: center;
+            height: 50px;
+            line-height: 50px;
+            border-radius:10px;
+        }
+        .cato:hover{
+            color: white;
+            background:hsla(0,0%,100%,.3);
+            text-decoration:none;
+        }
+    </style>
