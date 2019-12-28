81e072ad99a5581c9686abc81dda08995d60f524
Wei Wang <kaz749283@gmail.com>
2019-12-28 15:54:52 +0800

    update

--- app/Http/Controllers/replyController.php
+++ app/Http/Controllers/replyController.php
@@ -10,10 +10,10 @@ class replyController extends Controller
     public function store(Request $request)
     {
         $post= new reply;
-        $post->CommentNo=reply::max('ReplyNo')+1;
-        $post->CourseNo=$request->CourseNo;
+        $post->ReplyNo=reply::max('ReplyNo')+1;
+        $post->CommentNo=$request->CommentNo;
         $post->Account=Auth::user()->email;
-        $post->Comment=$request->Comment;
+        $post->Content=$request->Content;
         $post->save();
         return redirect()->action('Controller@personal',@Auth::user()->name);
     }
@@ -21,9 +21,9 @@ class replyController extends Controller
     public function update(Request $request, $id)
     {
         $post=reply::find($id);
-        $post->CourseNo=$request->CourseNo;
+        $post->CommentNo=$request->CommentNo;
         $post->Account=Auth::user()->email;
-        $post->Comment=$request->Comment;
+        $post->Content=$request->Content;
         $post->save();
         return redirect()->action('Controller@course',$request->CourseNo);
     }
