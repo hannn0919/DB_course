6e59aa640e52e7616d194d30d1f0a5ef081ec802
Wei Wang <kaz749283@gmail.com>
2019-12-28 20:07:34 +0800

    update

--- resources/views/showExp.blade.php
+++ resources/views/showExp.blade.php
@@ -8,32 +8,50 @@
 		<div class="card-header text-center font-weight-bold bg-white" style="border-color:#00324e;">
 			<div class="row">
 				<button onclick="history.back()" class="btn"><i class="fas fa-angle-double-left"></i></button>
-				<h3 class="col-10">心得</h3>
+				<div class="col-10">心得</div>
 			</div>
 		</div>
 		<div class="card-body" style="height: 79vh; overflow:auto">
-			<small class="float-right">作者: {{$user_array[$exp[0]->Account]}}</small>
+			<small class="float-right">作者: {{$user_array[$exp[0]->Account]}}</small><br>
 			<div>
-				<h3>課號：{{$exp[0]->CourseNo}}</h3><br>
-				<h3>課名：{{$course[0]->CourseTitle}}</h3><br>
-				<h3>開課系所：{{$course[0]->Department}}</h3><br>
-				<h3>授課老師：{{$course[0]->Instructor}}</h3><br>
-				<h3>開課年度：{{$exp[0]->Semester}}</h3><br>
-				<h3>推薦指數：<i class="fas fa-star" style="color:#FFD306;"></i> x {{$exp[0]->Score}}</h3><br>
+			  <div class="form-group row">
+				<label class="col-sm-2 col-form-label">* 課號</label>
+				<div class="col-sm-10">{{$exp[0]->CourseNo}}</div>
+			  </div>
+			  <div class="form-group row">
+				<label  class="col-sm-2 col-form-label">* 課名</label>
+				<div class="col-sm-10">{{$course[0]->CourseTitle}}</div>
+			  </div>
+			  <div class="form-group row">
+				<label  class="col-sm-2 col-form-label">* 開課系所</label>
+				<div class="col-sm-10">{{$course[0]->Department}}</div>
+			  </div>
+			  <div class="form-group row">
+				<label  class="col-sm-2 col-form-label">* 授課老師</label>
+				<div class="col-sm-10">{{$course[0]->Instructor}}</div>
+			  </div>
+			  <div class="form-group row">
+				<label class="col-sm-2 col-form-label">* 開課年度</label>
+				<div class="col-sm-10">{{$exp[0]->Semester}}</div>
+			  </div>
+			  <div class="form-group row">
+				<label  class="col-sm-2 col-form-label">* 推薦指數</label>
+				<div class="col-sm-10">{{$exp[0]->Score}}</div>
+			  </div>
 			</div>
 			<hr>
 			<div class="row">
-				<h3 class="col-auto">評分方式：</h3>
+				<div class="col-sm-2 col-form-label">評分方式：</div>
 				<textarea rows="4" readonly class="col" style="outline: none; font-size: 20px; border: none;">{{$exp[0]->Evaluation}}</textarea>
 			</div>
 			<br><hr>
 			<div class="row">
-				<h3 class="col-auto">考試與作業：</h3>
+				<div class="col-sm-2 col-form-label">考試與作業：</div>
 				<textarea rows="4" readonly class="col" style="outline: none; font-size: 20px; border: none;">{{$exp[0]->Outline}}</textarea>
 			</div>
 			<br><hr>
 			<div class="row">
-				<h3 class="col-auto">其他：</h3>
+				<div class="col-sm-2 col-form-label">心得：</div>
 				<textarea rows="4" readonly class="col" style="outline: none; font-size: 20px; border: none;">{{$exp[0]->Additional}}</textarea>
 			</div>
 
